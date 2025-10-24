3d7755ca5655a96bbc897dc38f1f87f8b304de86
kevin.thizy <kevin.thizy@outlook.fr>
2025-10-24 12:06:20 +0200

    ✨(layout) Enhance metadata with detailed description, keywords, and application info

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Joutes",
-  description: "Ligues et rencontres multi-jeux à proximité",
+  title: "Joutes - Ligues et rencontres multi-jeux à proximité",
+  description: "Ligues et rencontres multi-jeux à proximité pour les passionnés de jeux de cartes à collectionner et de jeux de société. Trouvez des événements organisés près de chez vous, connectez-vous avec votre communauté locale de joueurs, et découvrez des boutiques de jeux locales.",
+  applicationName: "Joutes App",
+  keywords: ["tcg", "board games", "events", "organized play", "local gaming community", "local game stores"],
+  creator: "Nakasar",
+  publisher: "Nakasar",
 };
 
 export default function RootLayout({
