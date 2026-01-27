c9b12134d11be0b73487401b1cb681caf5843c98
Nakasar <kevin.thizy@outlook.fr>
2026-01-27 15:04:55 +0000

    Fix url in auth routes

--- app/.well-known/oauth-protected-resource/[resource-path]/route.ts
+++ app/.well-known/oauth-protected-resource/[resource-path]/route.ts
@@ -2,8 +2,8 @@ import { serverClient } from "@/lib/server-client";
 
 export const GET = async () => {
   const metadata = await serverClient.getProtectedResourceMetadata({
-    resource: "https://joutes.app", // `aud` claim
-    authorization_servers: ["https://joutes.app"],
+    resource: "https://www.joutes.app", // `aud` claim
+    authorization_servers: ["https://www.joutes.app"],
   });
 
   return new Response(JSON.stringify(metadata), {
