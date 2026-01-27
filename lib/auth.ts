b8c90d867e3752936d53d63b2e5c1708bce51455
kevin.thizy <kevin.thizy@outlook.fr>
2026-01-27 23:01:24 +0100

    ✨ Update OAuth login page path and enhance consent page with session handling

--- lib/auth.ts
+++ lib/auth.ts
@@ -54,6 +54,7 @@ export const auth = betterAuth({
       consentPage: "/oauth/consent",
       allowDynamicClientRegistration: true,
       allowUnauthenticatedClientRegistration: true,
+      validAudiences: [process.env.NEXT_PUBLIC_BASE_URL || process.env.BETTER_AUTH_URL  || "http://localhost:3000"],
     }),
   ],
   session: {
@@ -73,7 +74,7 @@ export const auth = betterAuth({
     }),
   },
   trustedOrigins: process.env.NEXT_PUBLIC_BASE_URL ? [process.env.NEXT_PUBLIC_BASE_URL] : ["http://localhost:3000", "https://localhost:3000"],
-  baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || process.env.BETTER_AUTH_URL || "http://localhost:3000",
 });
 
 export type Session = typeof auth.$Infer.Session.session;
