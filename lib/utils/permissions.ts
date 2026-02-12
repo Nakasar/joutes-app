40950ededdc065cd804b80ef4169d03ee006082a
kevin.thizy <kevin.thizy@outlook.fr>
2026-02-12 23:16:49 +0100

    ✨ Ajouter la vérification si l'utilisateur est le créateur de l'événement dans la fonction isUserOrganizer

--- lib/utils/permissions.ts
+++ lib/utils/permissions.ts
@@ -2,5 +2,6 @@ import { Event } from "../types/Event";
 
 export function isUserOrganizer(event: Event, userId?: string): boolean {
   if (!event.staff || !userId) return false;
+  if (event.creatorId === userId) return true;
   return event.staff.some(s => s.userId === userId && s.role === 'organizer');
 }
