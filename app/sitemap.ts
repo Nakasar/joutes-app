c8fd16f259a029bdc35e06307c44ba4e8fc351b1
Nakasar <nakasar@outlook.fr>
2026-04-21 14:00:40 +0000

    ✨ Ajout des fichiers robots.ts et sitemap.ts pour la gestion des règles d'indexation et la génération de sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,42 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.joutes.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://www.joutes.app/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.joutes.app/games',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.joutes.app/lairs',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.joutes.app/events',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+    {
+      url: 'https://tools.joutes.app',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.4,
+    },
+  ];
+}
