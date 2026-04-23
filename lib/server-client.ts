392fb8b4d6da8efb3406b1300f2bb8d40d34864e
Nakasar <nakasar@outlook.fr>
2026-04-23 11:50:38 +0000

    ✨ refactor: mettre à jour les dépendances @better-auth à la version 1.6.8 et ajuster la configuration d'authentification

--- lib/server-client.ts
+++ lib/server-client.ts
@@ -3,5 +3,5 @@ import { createAuthClient } from "better-auth/client";
 import { oauthProviderResourceClient } from "@better-auth/oauth-provider/resource-client";
 
 export const serverClient = createAuthClient({
-  plugins: [oauthProviderResourceClient(auth)], // auth optional
+  plugins: [oauthProviderResourceClient()], // auth optional
 });
