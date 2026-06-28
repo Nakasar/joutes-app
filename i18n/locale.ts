66dfba15cad97407c950eca42652f5d0418ee5d5
kevin.thizy <kevin.thizy@outlook.fr>
2026-06-28 17:19:24 +0200

    ✨(metadata) Update description for improved clarity on community events and rules

--- i18n/locale.ts
+++ i18n/locale.ts
@@ -1,14 +1,37 @@
 "use server";
 
-import { cookies } from "next/headers";
-import { Locale, defaultLocale } from "@/i18n/config";
+import {cookies, headers} from "next/headers";
+import {Locale, defaultLocale, locales} from "@/i18n/config";
+import Negotiator from "negotiator";
+import {match} from "@formatjs/intl-localematcher";
 
 // In this example the locale is read from a cookie. You could alternatively
 // also read it from a database, backend service, or any other source.
 const COOKIE_NAME = "NEXT_LOCALE";
 
 export async function getUserLocale() {
-  return (await cookies()).get(COOKIE_NAME)?.value || defaultLocale;
+  const locale = (await cookies()).get(COOKIE_NAME)?.value;
+
+  if (locale) {
+    return locale;
+  }
+
+  if (!locale) {
+    const h = await headers();
+    const acceptLanguages = h.get('accept-language');
+
+    if (!acceptLanguages) {
+      return defaultLocale;
+    }
+
+    const languages = new Negotiator({
+      headers: {
+        'accept-language': acceptLanguages,
+      }
+    }).languages()
+
+    return match(languages, locales, defaultLocale)
+  }
 }
 
 export async function setUserLocale(locale: Locale) {
--- i18n/request.ts
+++ i18n/request.ts
@@ -1,8 +1,13 @@
 import {getRequestConfig} from 'next-intl/server';
 import {getUserLocale} from "@/i18n/locale";
+import Negotiator from 'negotiator';
+import {headers} from "next/headers";
+import {match} from "@formatjs/intl-localematcher";
+import {defaultLocale, locales} from "@/i18n/config";
+
 
 export default getRequestConfig(async () => {
-  const locale = await getUserLocale();
+  let locale = await getUserLocale();
 
   return {
     locale,
