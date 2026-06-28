fe96827f394fedfb3ddceb02a3f050f85108f05a
kevin.thizy <kevin.thizy@outlook.fr>
2026-06-28 15:33:05 +0200

    ✨(localization) Add multilingual support for TR and CR documents in search functionality

--- app/api/games/[gameId]/rules/route.ts
+++ app/api/games/[gameId]/rules/route.ts
@@ -2,6 +2,8 @@ import {NextRequest, NextResponse} from "next/server";
 import db from "@/lib/mongodb";
 import cr from '@/data/riftbound/cr.json';
 import tr from '@/data/riftbound/tr.json';
+import fr_cr from '@/data/riftbound/fr/cr.json';
+import fr_tr from '@/data/riftbound/fr/tr.json';
 import {Game} from "@/lib/types/Game";
 
 export async function GET(request: NextRequest, { params }: { params: Promise<{ gameId: string }> }) {
@@ -17,19 +19,51 @@ export async function GET(request: NextRequest, { params }: { params: Promise<{ 
   }
 
   const searchParams = new URL(request.url).searchParams;
-  const document = searchParams.get('document') || undefined;
+  const documentRaw = searchParams.get('document') ?? undefined;
+  const lang = searchParams.get('lang') ?? 'en';
+  const ruleId = searchParams.get('ruleId') ?? undefined;
 
-  if (!document) {
+  if (!documentRaw) {
     return NextResponse.json({ error: "missing source document in query" }, { status: 400 });
   }
+
+  const document = documentRaw.toUpperCase();
 
   if (!['TR', 'CR'].includes(document)) {
     return NextResponse.json({ error: `allowed document sources in query for ${game.name}: TR, CR` });
   }
 
   if (document === 'TR') {
-    return NextResponse.json(tr);
+    if (lang === 'fr') {
+      if (ruleId) {
+        const rule = fr_tr.find(r => r.id === ruleId);
+        return NextResponse.json([rule]);
+      }
+      return NextResponse.json(fr_tr);
+    } else if (lang === 'en') {
+      if (ruleId) {
+        const rule = tr.find(r => r.id === ruleId);
+        return NextResponse.json([rule]);
+      }
+      return NextResponse.json(tr);
+    } else {
+      return NextResponse.json({ error: `allowed lang in query for ${document} in ${game.name}: en, fr` });
+    }
   } else if (document === 'CR') {
-    return NextResponse.json(cr);
+    if (lang === 'fr') {
+      if (ruleId) {
+        const rule = fr_cr.find(r => r.id === ruleId);
+        return NextResponse.json([rule]);
+      }
+      return NextResponse.json(fr_cr);
+    } else if (lang === 'en') {
+      if (ruleId) {
+        const rule = cr.find(r => r.id === ruleId);
+        return NextResponse.json([rule]);
+      }
+      return NextResponse.json(cr);
+    } else {
+      return NextResponse.json({ error: `allowed lang in query for ${document} in ${game.name}: en, fr` });
+    }
   }
 }
