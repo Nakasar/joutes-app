f6b68b6551c44e0da36d4ff16eb7fe8850b3d593
kevin.thizy <kevin.thizy@outlook.fr>
2026-06-30 14:32:48 +0200

    ✨(discord) Add events-board

--- app/api/cron/update-discord-boards/route.ts
+++ app/api/cron/update-discord-boards/route.ts
@@ -3,7 +3,7 @@ import db from "@/lib/mongodb";
 import {REST} from "@discordjs/rest";
 import {ButtonStyle, Routes} from "discord-api-types/v10";
 import {DiscordBoard} from "@/app/discord/route";
-import {getEventsByLairId} from "@/lib/db/events";
+import {getEventsByLairId, getEventsByLairIds} from "@/lib/db/events";
 import {DateTime} from "luxon";
 import {ActionRowBuilder, ButtonBuilder, EmbedBuilder} from "@discordjs/builders";
 import {DiscordEmojis} from "@/app/discord/utils";
@@ -24,47 +24,46 @@ export async function GET(req: Request) {
 
     if (await boardsToUpdateCursor.hasNext()) {
       const board = await boardsToUpdateCursor.next();
-      if (board && board.lairs[0] && board.games[0]) {
+      if (board && board.lairs.length > 0 && board.games.length > 0) {
         console.log(`Updating board ${board.messageId}`);
 
         const currentDate = DateTime.utc();
 
-        const lair = await getLairById(board.lairs[0].toString())
-        const game = await getGameById(board.games[0].toString())
-        if (lair && game) {
-          const events = await getEventsByLairId(board.lairs[0].toString(), {
-            gameId: board.games[0].toString(),
-            year: currentDate.year,
-            month: currentDate.month,
-          });
+        const events = await getEventsByLairIds(board.lairs.map(id => id.toString()), {
+          gameIds: board.games.map(id => id.toString()),
+          year: currentDate.year,
+          month: currentDate.month,
+        });
 
-          await rest.patch(Routes.channelMessage(
-            board.channelId,
-            board.messageId,
-          ), {
-            body: {
-              embeds: [
-                new EmbedBuilder()
-                  .setTitle(`Events - ${lair.name}`)
-                  .setDescription(`Voici les évènements à venir :\n\n${events.length > 0 ? events.map(e => `-${e.game?.slug ? ` <:${e.game.slug}:${DiscordEmojis[e.game.slug] ?? ''}>` : ''} [${e.name}](https://joutes.app/events/${e.id}) le ${DateTime.fromISO(e.startDateTime, { zone: 'Europe/Paris', locale: 'fr' }).toLocaleString(DateTime.DATETIME_MED)}`).join('\n') : 'Aucun évènement à venir.'}`)
-                  .setFooter({
-                    text: `Updated: ${currentDate.setZone('Europe/Paris').toLocaleString(DateTime.DATETIME_MED, { locale: 'fr' })}`,
-                  }),
-              ],
-              content: null,
-              components: [
-                new ActionRowBuilder<ButtonBuilder>().addComponents(
-                  new ButtonBuilder()
-                    .setLabel("Modifier")
-                    .setCustomId(`modify-events-board-${board._id.toString()}`)
-                    .setStyle(ButtonStyle.Secondary),
-                ),
-              ]
-            },
-          }).catch(err => {
-            console.warn('Failed to update board message (might have been deleted)');
-          });
-        }
+        await rest.patch(Routes.channelMessage(
+          board.channelId,
+          board.messageId,
+        ), {
+          body: {
+            embeds: [
+              new EmbedBuilder()
+                .setTitle(`Events`)
+                .setDescription(`Voici les évènements à venir :\n\n${events.length > 0 ? events.map(e => `-${e.game?.slug ? ` <:${e.game.slug}:${DiscordEmojis[e.game.slug] ?? ''}>` : ''} [${e.name}](https://joutes.app/events/${e.id}) le ${DateTime.fromISO(e.startDateTime, {
+                  zone: 'Europe/Paris',
+                  locale: 'fr'
+                }).toLocaleString(DateTime.DATETIME_MED)} à ${e.lair?.name ?? 'Lieu Inconnu'}`).join('\n') : 'Aucun évènement à venir.'}`)
+                .setFooter({
+                  text: `Updated: ${currentDate.setZone('Europe/Paris').toLocaleString(DateTime.DATETIME_MED, { locale: 'fr' })}`,
+                }),
+            ],
+            content: null,
+            components: [
+              new ActionRowBuilder<ButtonBuilder>().addComponents(
+                new ButtonBuilder()
+                  .setLabel("Modifier")
+                  .setCustomId(`modify-events-board-${board._id.toString()}`)
+                  .setStyle(ButtonStyle.Secondary),
+              ),
+            ]
+          },
+        }).catch(err => {
+          console.warn('Failed to update board message (might have been deleted)');
+        });
       }
     }
 
