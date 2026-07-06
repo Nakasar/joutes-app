f300a6de9cd15f6c2e05e08e4295e9033886c09f
kevin.thizy <kevin.thizy@outlook.fr>
2026-07-06 22:08:26 +0200

    ✨(discord) Implement weekly email notifications for users

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -2,7 +2,8 @@ export function GET() {
   const content = `User-Agent: *
 Allow: /
 Disallow: /admin/
-Sitemap: https://www.joutes.app/sitemap.xml`;
+Sitemap: https://www.joutes.app/sitemap.xml
+Sitemap: https://www.joutes.app/sitemap_index.xml`;
 
   return new Response(content, {
     headers: {
