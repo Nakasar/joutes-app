a75f094dfcf294570f11b9098b363e603c958d78
kevin.thizy <kevin.thizy@outlook.fr>
2026-07-11 00:38:11 +0200

    ✨(import) Enhance card import functionality with file reading and batch processing

--- scripts/games/swu/import-cards.ts
+++ scripts/games/swu/import-cards.ts
@@ -1,65 +1,163 @@
-import db from "@/lib/mongodb";
+import {readFileSync, writeFileSync} from "node:fs";
+import path from "node:path";
 import {ObjectId} from "mongodb";
+import db from "@/lib/mongodb";
+import meilisearch, {indexes} from "@/lib/meilisearch";
+import {randomUUID} from "node:crypto";
 
-/*
- Fetch SWU cards from API and import them into meilisearch and mongodb.
- */
-
-type CardAPI = {
+export type Card = {
   id: string;
-  setId: string;
-  number: string;
-  name: string;
-  displayName: string;
-  subtitle?: string;
-  type: string;
-  traits: string;
-  text: string;
-}
+  imageUrl: string;
+  lang: string;
+  setCode: "string";
+  collectorNumber: "string";
+  name: "string";
+  subtitle: "string";
+  traits: string[];
+  type: "string";
+  arenas: string[];
+  text: "string";
+  price?: "string";
+  foilPrice?: "string";
+  rarity: "string";
+  cost: number;
+  hp: number;
+  power: number;
+};
 
-async function getCardsFromAPI(): Promise<CardAPI[]> {
-  console.info('Importing cards database from API...');
+export async function importCardDatabase() {
+  console.info('Importing cards database...');
 
-  const cardsResult = await fetch('https://api.dotgg.gg/cgfw/getcards?game=starwars');
+  const cardsIndex = meilisearch.index(indexes.swu.name);
+  await cardsIndex.deleteAllDocuments();
 
-  if (!cardsResult.ok) {
-    throw new Error('Failed to fetch cards database');
-  }
 
-  const cardsRaw = await cardsResult.json();
-
-  return cardsRaw;
-}
-
-async function main() {
-  const cardsRaw = await getCardsFromAPI();
-
-  const cards = cardsRaw.map(card => {
-    return {
-      originalId: card.id,
-      id: `${card.setId}${card.number}`,
-      name: card.displayName,
-      type: card.type,
-      text: card.text,
-      subtitle: card.text,
-      title: card.name,
-    };
+  await db.collection('cards').deleteMany({
+    gameId: new ObjectId('68f108675fdfb9c53ba3387d'),
   });
 
-  for (const card of cards) {
-    console.log(card.originalId, card.id);
-    await db.collection('cards').updateOne({
-      id: card.originalId,
-      gameId: new ObjectId('68f108675fdfb9c53ba3387d'),
-    }, {
-      $set: { id: card.id },
+  const cardsRaw = await getCardsListFromFile();
+
+  let cards = [];
+
+  const batchSize = 50000;
+
+  for (const cardRaw of cardsRaw) {
+    cards.push({
+      id: randomUUID(),
+      image: cardRaw.imageUrl,
+      lang: cardRaw.lang,
+      setCode: cardRaw.setCode,
+      collectorNumber: cardRaw.collectorNumber,
+      name: cardRaw.name,
+      subtitle: cardRaw.name,
+      traits: cardRaw.traits,
+      type: cardRaw.type,
+      arenas: cardRaw.arenas,
+      text: cardRaw.text,
+      price: cardRaw.price,
+      foilPrice: cardRaw.foilPrice,
+      rarity: cardRaw.rarity,
+      cost: cardRaw.cost,
+      hp: cardRaw.hp,
+      power: cardRaw.power,
     });
+
+    if (cards.length >= batchSize) {
+      await cardsIndex.addDocuments(cards);
+      await db.collection('cards').insertMany(cards.map(c => ({
+        ...c,
+        gameId: new ObjectId('68f108675fdfb9c53ba3387d'),
+      })));
+      cards = [];
+
+      await new Promise((resolve) => setTimeout(resolve, 60000));
+    }
   }
+
+  if (cards.length > 0) {
+    await cardsIndex.addDocuments(cards);
+    await db.collection('cards').insertMany(cards.map(c => ({
+      ...c,
+      gameId: new ObjectId('68f108675fdfb9c53ba3387d'),
+    })));
+  }
+
+  console.info('Cards database imported!')
 }
 
-main().then(() => {
+async function getCardsListFromFile(): Promise<Card[]> {
+  const cardsRaw: any[] = JSON.parse(readFileSync(path.join(__dirname, './cards.json')).toString());
+
+  return cardsRaw.map(cardRaw => ({
+    id: cardRaw.id,
+    name: cardRaw.attributes.title,
+    subtitle: cardRaw.attributes.subtitle,
+    setCode: cardRaw.attributes.expansion.data.attributes.code,
+    lang: cardRaw.attributes.locale,
+    text: cardRaw.attributes.text,
+    power: cardRaw.attributes.power,
+    hp: cardRaw.attributes.hp,
+    cost: cardRaw.attributes.cost,
+    type: cardRaw.attributes.type.data.attributes.value,
+    traits: cardRaw.attributes.traits.data.map((trait: any) => trait.attributes.name),
+    arenas: cardRaw.attributes.arenas.data.map((arena: any) => arena.attributes.name),
+    rarity: cardRaw.attributes.rarity.data.attributes.englishName,
+    imageUrl: cardRaw.attributes.artFront.data?.attributes.url,
+    collectorNumber: cardRaw.attributes.cardNumber,
+  }))
+}
+
+async function getCardsListFromOfficialWebSite(): Promise<Card[]> {
+  const cards = [];
+
+  let page = 1;
+
+  while (true) {
+    console.info(`Fetching cards database page ${page}...`);
+    const cardsResult = await fetch(`https://admin.starwarsunlimited.com/api/card-list?locale=fr&pagination[page]=${page}&pagination[pageSize]=100`);
+
+    if (!cardsResult.ok) {
+      console.error('Failed to fetch cards database');
+      console.error(await cardsResult.text());
+      throw new Error('Failed to fetch cards database');
+    }
+
+    const json = await cardsResult.json();
+
+    cards.push(...json.data);
+
+    if (json.meta.pagination.page >= json.meta.pagination.pageCount || json.data.length === 0) {
+      break;
+    }
+
+    page++;
+  }
+
+  writeFileSync(path.join(__dirname, './cards.json'), JSON.stringify(cards));
+
+  // const cardsRaw = JSON.parse(readFileSync('/Users/kevin.thizy/Documents/Personnel/swu-collection-tools/tmp/data/cards.json').toString());
+  // cards.push(...cardsRaw);
+
+  return cards.map(cardRaw => ({
+    id: cardRaw.id,
+    name: cardRaw.attributes.title,
+    subtitle: cardRaw.attributes.subtitle,
+    setCode: cardRaw.attributes.expansion.data.attributes.code,
+    lang: cardRaw.attributes.locale,
+    text: cardRaw.attributes.text,
+    power: cardRaw.attributes.power,
+    hp: cardRaw.attributes.hp,
+    cost: cardRaw.attributes.cost,
+    type: cardRaw.attributes.type.data.attributes.value,
+    traits: cardRaw.attributes.traits.data.map((trait: any) => trait.attributes.name),
+    arenas: cardRaw.attributes.arenas.data.map((arena: any) => arena.attributes.name),
+    rarity: cardRaw.attributes.rarity.data.attributes.englishName,
+    imageUrl: cardRaw.attributes.artFront.data?.attributes.url,
+    collectorNumber: cardRaw.attributes.cardNumber,
+  }))
+}
+
+importCardDatabase().then(() => {
   process.exit(0);
-}).catch(err => {
-  console.error(err);
-  process.exit(1);
 });
