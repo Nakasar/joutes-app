3cc525629e1dda399ec1514f4d5d835f26a2c897
Nakasar <nakasar@outlook.fr>
2026-07-27 10:06:12 +0200

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -88,7 +88,17 @@ export const auth = betterAuth({
     },
   },
   trustedOrigins: process.env.NEXT_PUBLIC_BASE_URL ? [process.env.NEXT_PUBLIC_BASE_URL, "http://tauri.localhost", "https://tauri.localhost"] : ["http://localhost:3000", "https://localhost:3000", "http://tauri.localhost", "https://tauri.localhost"],
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL || process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: {
+    allowedHosts: [
+			"joutes.app",
+			"*.joutes.app",
+			"*.vercel.app",
+      process.env.BETTER_AUTH_URL,
+      process.env.NEXT_PUBLIC_BASE_URL,
+		],
+		protocol: "https",
+		fallback: "https://www.joutes.app",
+  },
 });
 
 export type Session = typeof auth.$Infer.Session.session;
